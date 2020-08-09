8d5cdc80560043d0804fa8247a43001dd0d0712f
= <me@alenkart.com>
2020-08-09 09:09:55 -0400

    First commit

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Jewel",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Jewel",
+            type: .dynamic,
             targets: ["Jewel"]),
     ],
     dependencies: [
