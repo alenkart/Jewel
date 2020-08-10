51efaa4dec6c6024b38b3f7d26ad59fe56b06944
= <me@alenkart.com>
2020-08-10 19:59:55 -0400

    Added shimmer view

--- Tests/JewelTests/JewelTests.swift
+++ Tests/JewelTests/JewelTests.swift
@@ -1,15 +1,2 @@
 import XCTest
 @testable import Jewel
-
-final class JewelTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Jewel().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
-}
--- Tests/JewelTests/XCTestManifests.swift
+++ Tests/JewelTests/XCTestManifests.swift
@@ -1,9 +1,1 @@
 import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(JewelTests.allTests),
-    ]
-}
-#endif
